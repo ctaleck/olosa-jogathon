15f23a1714f2c5974bc2417eb6fe0064b56f3af7
Christopher Taleck <chrisweb@taleck.com>
2018-09-21 10:42:46 -0700

    Update live email and url

--- urls.js
+++ urls.js
@@ -1,15 +1,16 @@
 var sandbox = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
 var prod = 'https://www.paypal.com/cgi-bin/webscr';
 var item = 'OLOS+Academy+2018+Jog-a-thon';
-var email = 'treasurer@olosa.org';
+var emailSchool = 'treasurer@olosa.org';
+var email = 'ourladyofsorrows@sspx.org';
 var emailSandbox = 'chris-facilitator@taleck.com';
 var href = [
     'https://www.paypal.com/verified/pal=', email
 ].join('');
 
 var urls = {
-    paypalBase: sandbox,
-    paypalEmail: emailSandbox,
+    paypalBase: prod,
+    paypalEmail: email,
     paypalItem: item,
     paypalEmailVerifyHref: null // href
 };
