487a800e2cf907398048cb47e24fd4f25adb30ca
Christopher Taleck <chris@taleck.com>
2018-09-20 20:55:24 -0700

    Update to sandbox urls

--- urls.js
+++ urls.js
@@ -1,14 +1,15 @@
 var sandbox = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
 var prod = 'https://www.paypal.com/cgi-bin/webscr';
 var item = 'OLOS+Academy+2018+Jog-a-thon';
-var email = 'treasure@olosa.org';
+var email = 'treasurer@olosa.org';
+var emailSandbox = 'chris-facilitator@taleck.com';
 var href = [
     'https://www.paypal.com/verified/pal=', email
 ].join('');
 
 var urls = {
     paypalBase: sandbox,
-    paypalEmail: email,
+    paypalEmail: emailSandbox,
     paypalItem: item,
     paypalEmailVerifyHref: null // href
 };
