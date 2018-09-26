3531dbc5f44941eb9db5297ae08c72a6dbd59062
Monique Taleck <monique@st-cosmos.local>
2018-09-25 23:01:57 -0700

    Updates text and adds confirm page (not used)

--- urls.js
+++ urls.js
@@ -1,6 +1,6 @@
 var sandbox = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
 var prod = 'https://www.paypal.com/cgi-bin/webscr';
-var item = 'OLOS+Academy+2018+Jog-a-thon';
+var item = 'OLOS+Academy+2018+Jog-A-Thon';
 var emailSchool = 'treasurer@olosa.org';
 var email = 'ourladyofsorrows@sspx.org';
 var emailSandbox = 'chris-facilitator@taleck.com';
